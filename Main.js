37354971d775802a078b16159a335772591fc914
8329441266 <sadashivingale2810@gmail.com>
2020-12-25 13:45:43 -0800

    check part time or full time emp wage

--- Main.js
+++ Main.js
@@ -2,3 +2,4 @@ console.log("...WELCOOME TO EMPLOYEE WAGE...");
 const util = require("./Utility");
 util.empAttendance();
 util.dailyEmpWage();
+util.empPart_Fullwage();
--- Utility.js
+++ Utility.js
@@ -3,6 +3,8 @@ class Utility {
     EMP_RATE_PER_HOUR = 20;
     NUM_OF_WORKING_DAYS = 25;
     MAX_HRS_IN_MONTH = 100;
+    IS_PART_TIME = 2;
+    IS_FULL_TIME = 1;
     empHrs = 0;
 
     empAttendance = () => {
@@ -21,6 +23,20 @@ class Utility {
         this.empAttendance ();
         let empWage = this.empHrs * this.EMP_RATE_PER_HOUR;
         console.log('The Daily Eamployee Wage Is : ' +empWage);
+    }
+
+    empPart_Fullwage = () =>{
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        
+        if (empCheck == this.IS_PART_TIME){
+            let empHrs = 4;
+            let empWage = empHrs * this.EMP_RATE_PER_HOUR;
+            console.log('Part Time Of Eamployee Is : ' +empWage);
+        }
+        else if (empCheck == this.IS_FULL_TIME)
+            this.dailyEmpWage();
+        else 
+            console.log('Employee Is Absent.');
     }
     
 }
